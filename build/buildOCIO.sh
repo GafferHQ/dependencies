71672e7490595bcfcbd50b39ec4bd6898d13ce6a
John Haddon <thehaddonyoof@gmail.com>
2015-04-25 13:03:17 +0100

    Added OCIO build script.

---
+++ build/buildOCIO.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+pushd `dirname $0`/../imageworks-OpenColorIO-8883824
+
+cmake -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -DOCIO_BUILD_TRUELIGHT=OFF -DOCIO_BUILD_APPS=OFF -DOCIO_BUILD_NUKE=OFF
+make clean && make -j 4 && make install
+
+mkdir -p $BUILD_DIR/python
+mv $BUILD_DIR/lib/python*/site-packages/PyOpenColorIO* $BUILD_DIR/python
+
+popd
+
+mkdir -p $BUILD_DIR/openColorIO
+cp `dirname $0`/../imageworks-OpenColorIO-Configs-f931d77/nuke-default/config.ocio $BUILD_DIR/openColorIO
+cp -r `dirname $0`/../imageworks-OpenColorIO-Configs-f931d77/nuke-default/luts $BUILD_DIR/openColorIO
