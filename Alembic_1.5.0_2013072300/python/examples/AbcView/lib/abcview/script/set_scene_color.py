50dc4fbeea1e7847681eb2b8b4659e567b7614a9
John Haddon <thehaddonyoof@gmail.com>
2015-04-30 09:25:49 +0100

    Updated to Alembic 1.5.8.

--- Alembic_1.5.0_2013072300/python/examples/AbcView/lib/abcview/script/set_scene_color.py
+++
@@ -1,6 +0,0 @@
-from PyQt4 import QtGui
-from abcview.io import Mode
-color = QtGui.QColorDialog(self).getColor()
-item = objects.selectedItems()[0]
-item.object.properties["color"] = (color.red(), color.green(), color.blue())
-self.viewer.updateGL()
