1a3bcd5efa180f5c3cfca3d6a1c01cfa81250a89
John Haddon <thehaddonyoof@gmail.com>
2015-05-06 18:28:53 +0100

    Added JPEG license installation.

--- build/buildJPEG.sh
+++ build/buildJPEG.sh
@@ -3,4 +3,8 @@
 set -e
 
 cd `dirname $0`/../jpeg-8c
+
+mkdir -p $BUILD_DIR/doc/licenses
+cp README $BUILD_DIR/doc/licenses/libjpeg
+
 ./configure --prefix=$BUILD_DIR && make clean && make && make install
