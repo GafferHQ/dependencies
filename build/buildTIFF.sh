a4622ac0744fc8f0ef652343c99cefd98e8b19a0
John Haddon <thehaddonyoof@gmail.com>
2015-05-11 15:49:19 +0100

    Fixed TIFF build - it was picking up the wrong libjpeg.

--- build/buildTIFF.sh
+++ build/buildTIFF.sh
@@ -4,6 +4,12 @@ set -e
 
 cd `dirname $0`/../tiff-3.8.2
 
+# needed to make sure we link against the libjpeg
+# in the gaffer distribution and not the system
+# libjpeg.
+export CPPFLAGS=-I$BUILD_DIR/include
+export LDFLAGS=-L$BUILD_DIR/lib
+
 mkdir -p $BUILD_DIR/doc/licenses
 cp COPYRIGHT $BUILD_DIR/doc/licenses/libtiff
 
