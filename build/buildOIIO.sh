7bbbc62f108aa56dd849aded88113ea94b13d25d
John Haddon <thehaddonyoof@gmail.com>
2015-06-23 11:46:34 +0100

    Fixed OIIO build for latest OIIO master.
    This now disallows building in the main directory, so we must make our own build directory.

--- build/buildOIIO.sh
+++ build/buildOIIO.sh
@@ -7,11 +7,14 @@ cd `dirname $0`/../oiio-Release-1.5.13
 mkdir -p $BUILD_DIR/doc/licenses
 cp LICENSE $BUILD_DIR/doc/licenses/openImageIO
 
+mkdir -p gafferBuild
+cd gafferBuild
+
 rm -f CMakeCache.txt
 
 cmake \
 	-D CMAKE_INSTALL_PREFIX=$BUILD_DIR \
 	-D CMAKE_PREFIX_PATH=$BUILD_DIR \
-	.
+	..
 
 make && make install
