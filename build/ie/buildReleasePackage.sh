03cb32c617ffe33b7b06054d7833366f2f3a3c4b
John Haddon <john@image-engine.com>
2015-11-19 14:30:43 -0800

    Add script for building dependencies at Image Engine.

---
+++ build/ie/buildReleasePackage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script builds the release package in the Image Engine environment.
+# It should be run from a CentOS 6 machine.
+
+set -e
+
+# Simplify path to ignore IE wrappers.
+export PATH=/software/apps/scons/2.0.1/bin/:/software/apps/doxygen/1.8.4/cent6.x86_64/bin:/software/apps/cmake/2.8.4/cent6.x86_64/bin:/usr/local/bin:/usr/bin:/bin
+
+export RMAN_ROOT=/software/apps/3delight/10.0.49/cent6.x86_64
+export ARNOLD_ROOT=/software/apps/arnold/4.1.2.0/cent6.x86_64
+
+export BUILD_DIR=$HOME/gafferDependencies/build-$VERSION
+
+`dirname $0`/../buildAll.sh
+`dirname $0`/../buildPackage.sh
