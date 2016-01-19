ff6e216bc48372e756024f507a1369349b86fb7b
John Haddon <john@image-engine.com>
2016-01-19 06:12:06 -0800

    IE build script : Use modern 3delight
    The previous version didn't have RiProceduralV defined.

--- build/ie/buildReleasePackage.sh
+++ build/ie/buildReleasePackage.sh
@@ -7,7 +7,7 @@ set -e
 # Simplify path to ignore IE wrappers.
 export PATH=/software/apps/scons/2.0.1/bin/:/software/apps/doxygen/1.8.4/cent6.x86_64/bin:/software/apps/cmake/2.8.4/cent6.x86_64/bin:/usr/local/bin:/usr/bin:/bin
 
-export RMAN_ROOT=/software/apps/3delight/10.0.49/cent6.x86_64
+export RMAN_ROOT=/software/apps/3delight/12.0.25/cent6.x86_64
 export ARNOLD_ROOT=/software/apps/arnold/4.1.2.0/cent6.x86_64
 
 export BUILD_DIR=$HOME/gafferDependencies/build-$VERSION
