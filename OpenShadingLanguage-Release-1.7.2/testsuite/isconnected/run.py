271857805b65394bc1764f47b4cd275789358b90
John Haddon <thehaddonyoof@gmail.com>
2016-12-30 20:54:38 +0000

    Update OpenShadingLanguage to 1.7.5.

--- OpenShadingLanguage-Release-1.7.2/testsuite/isconnected/run.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-command = testshade("-g 1 1 --layer upstream upstream --layer downstream test " +
-                    "--connect upstream out downstream a " +
-                    "--connect upstream struct1 downstream mystruct1")
