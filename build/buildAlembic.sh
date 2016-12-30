884650f05288c2212eb5e10dcd24bb16d5355479
John Haddon <thehaddonyoof@gmail.com>
2016-12-30 23:17:00 +0000

    Update Alembic to 1.6.1

--- build/buildAlembic.sh
+++ build/buildAlembic.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-cd `dirname $0`/../alembic-1.5.8
+cd `dirname $0`/../alembic-1.6.1
 
 mkdir -p $BUILD_DIR/doc/licenses
 cp LICENSE.txt $BUILD_DIR/doc/licenses/alembic
-
-# may need to hand edit build/AlembicBoost.cmake in the alembic distribution to remove Boost_USE_STATIC_LIBS.
-# may also need to set ALEMBIC_NO_TESTS=TRUE on OSX (in CMakeLists.txt).
 
 rm -f CMakeCache.txt
 cmake \
@@ -18,17 +15,16 @@ cmake \
 	-D Boost_NO_BOOST_CMAKE=TRUE \
 	-D BOOST_ROOT=$BUILD_DIR \
 	-D ILMBASE_ROOT=$BUILD_DIR \
+	-D HDF5_ROOT=$BUILD_DIR \
+	-D USE_HDF5=TRUE \
 	-D USE_PYILMBASE=FALSE \
 	-D USE_PYALEMBIC=FALSE \
 	-D USE_ARNOLD=FALSE \
 	-D USE_PRMAN=FALSE \
 	-D USE_MAYA=FALSE \
+	-D ALEMBIC_LIB_USES_BOOST=TRUE \
 	.
 
 make clean
 make
 make install
-
-rm -rf $BUILD_DIR/include/Alembic
-mv $BUILD_DIR/alembic-*/include/Alembic $BUILD_DIR/include
-mv $BUILD_DIR/alembic-*/lib/static/* $BUILD_DIR/lib
