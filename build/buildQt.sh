6c497cbcf5601cb52a44e8ceb866e27d1257237a
John Haddon <thehaddonyoof@gmail.com>
2017-01-04 17:53:13 +0000

    Fix Qt build on macOS Sierra
    Borrowed from https://github.com/vfxpro99/usd-build-club

--- build/buildQt.sh
+++ build/buildQt.sh
@@ -2,19 +2,25 @@
 
 set -e
 
-cd `dirname $0`/../qt-everywhere-opensource-src-4.8.5
+cd `dirname $0`/../qt-everywhere-opensource-src-4.8.7
 
 mkdir -p $BUILD_DIR/doc/licenses
 cp LICENSE.LGPL $BUILD_DIR/doc/licenses/qt
 
 export LD_LIBRARY_PATH=$BUILD_DIR/lib
 
+if [[ `uname` = "Darwin" ]] ; then
+	extraArgs="-arch x86_64 -platform unsupported/macx-clang"
+fi
+
 ./configure \
 	-prefix $BUILD_DIR \
 	-opensource -confirm-license \
 	-no-rpath -no-declarative -no-gtkstyle -no-qt3support \
-	-no-multimedia -no-audio-backend -no-script -no-dbus -no-svg \
-	-nomake examples -nomake demos -nomake tools \
+	-no-phonon -no-multimedia -no-audio-backend -no-dbus -no-svg \
+	-nomake examples -nomake demos -nomake tools -nomake docs -nomake translations \
+	$extraArgs \
+	-v \
 	-I $BUILD_DIR/include -L $BUILD_DIR/lib
 
 make -j 4 && make install
