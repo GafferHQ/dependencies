dc39c7dd0ff3183dbb7c41d5bf8240ee3ecd209a
John Haddon <thehaddonyoof@gmail.com>
2017-04-21 14:06:57 +0100

    Freetype : Fix linking with libpng

--- build/buildFreeType.sh
+++ build/buildFreeType.sh
@@ -7,4 +7,6 @@ cd `dirname $0`/../freetype-2.7.1
 mkdir -p $BUILD_DIR/doc/licenses
 cp docs/FTL.TXT $BUILD_DIR/doc/licenses/freetype
 
+export LDFLAGS=-L$BUILD_DIR/lib
+
 ./configure --prefix=$BUILD_DIR && make clean && make && make install
