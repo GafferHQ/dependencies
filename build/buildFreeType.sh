82303e0d60f1840ddf8fdbe2ae11d5814a8c7358
John Haddon <thehaddonyoof@gmail.com>
2017-04-25 11:30:18 +0100

    Freetype : Find libpng-config in $BUILD_DIR/bin
    This stops us falling through to pick up the png config from the system.

--- build/buildFreeType.sh
+++ build/buildFreeType.sh
@@ -7,6 +7,7 @@ cd `dirname $0`/../freetype-2.7.1
 mkdir -p $BUILD_DIR/doc/licenses
 cp docs/FTL.TXT $BUILD_DIR/doc/licenses/freetype
 
+export PATH=$BUILD_DIR/bin:$PATH
 export LDFLAGS=-L$BUILD_DIR/lib
 
 ./configure --prefix=$BUILD_DIR && make clean && make && make install
