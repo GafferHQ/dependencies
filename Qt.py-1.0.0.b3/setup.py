c65ddeafd81309e088c73b4456d8f5f763250561
John Haddon <thehaddonyoof@gmail.com>
2017-05-11 18:26:57 +0100

    Add Qt.py version 1.0.0b3

---
+++ Qt.py-1.0.0.b3/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+os.environ["QT_PREFERRED_BINDING"] = "None"
+version = __import__("Qt").__version__
+
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.5",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Utilities"
+]
+
+
+setup(
+    name="Qt.py",
+    version=version,
+    description="Python 2 & 3 compatibility wrapper around all Qt bindings - "
+                "PySide, PySide2, PyQt4 and PyQt5.",
+    author="Marcus Ottosson",
+    author_email="konstruktion@gmail.com",
+    url="https://github.com/mottosso/Qt",
+    license="MIT",
+    zip_safe=False,
+    py_modules=["Qt"],
+    classifiers=classifiers
+)
---
+++ build/buildQtPy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+cp `dirname $0`/../Qt.py-1.0.0.b3/Qt.py $BUILD_DIR/python
