b8a9f46eddbfd05af53e72115486369e1ded9fd3
John Haddon <thehaddonyoof@gmail.com>
2017-07-20 14:01:43 +0100

    OSL : Fix C++11/Clang header problem

--- build/buildOSL.sh
+++ build/buildOSL.sh
@@ -11,6 +11,17 @@ cp LICENSE $BUILD_DIR/doc/licenses/osl
 export DYLD_FALLBACK_LIBRARY_PATH=$BUILD_DIR/lib
 export LD_LIBRARY_PATH=$BUILD_DIR/lib
 
+if [[ `uname` = "Linux" ]] ; then
+	# OSL compiles llvm_ops.cpp using clang rather
+	# than g++. We need to make sure that clang uses
+	# the same headers as the main compiler. This is
+	# essential when building for C++11 on a system
+	# where the default GCC does not have C++11
+	# compatible headers.
+	gccRoot=$(which g++ | sed s/bin\\/g++//)
+	llvmCompileFlags="--gcc-toolchain=$gccRoot"
+fi
+
 mkdir -p gafferBuild
 cd gafferBuild
 
@@ -22,6 +33,7 @@ cmake \
 	-D CMAKE_PREFIX_PATH=$BUILD_DIR \
 	-D STOP_ON_WARNING=0 \
 	-D USE_CPP11=1 \
+	-D LLVM_COMPILE_FLAGS=$llvmCompileFlags \
 	..
 
 make VERBOSE=1 && make install
