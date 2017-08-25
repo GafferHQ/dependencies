2302526fcd0fa93799b899bbf5fe62845d09d1f5
John Haddon <thehaddonyoof@gmail.com>
2017-08-25 15:00:00 +0100

    JPEG : Update to libjpeg-turbo 1.5.2

--- build/buildJPEG.sh
+++ build/buildJPEG.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-cd `dirname $0`/../jpeg-8c
+cd `dirname $0`/../libjpeg-turbo-1.5.2
 
 mkdir -p $BUILD_DIR/doc/licenses
-cp README $BUILD_DIR/doc/licenses/libjpeg
+cp LICENSE.md $BUILD_DIR/doc/licenses/libjpeg-turbo
 
 ./configure --prefix=$BUILD_DIR && make clean && make && make install
