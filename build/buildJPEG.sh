d9104d658a99be41e6c42e1a16f735e66f073c08
John Haddon <thehaddonyoof@gmail.com>
2017-09-05 14:27:44 +0100

    JPEG : Add note

--- build/buildJPEG.sh
+++ build/buildJPEG.sh
@@ -7,4 +7,10 @@ cd `dirname $0`/../libjpeg-turbo-1.5.2
 mkdir -p $BUILD_DIR/doc/licenses
 cp LICENSE.md $BUILD_DIR/doc/licenses/libjpeg-turbo
 
+# Note :
+# You may need to run `autoreconf -ivf` if git checks out the files
+# with timestamps that convince autotools that things are not up to
+# date and need rerunning. Really we need to switch things so we
+# download tarballs rather than keep the code in git.
+
 ./configure --prefix=$BUILD_DIR && make clean && make && make install
