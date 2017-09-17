ea74e96069d828ad80ec83e5029dcc38e912f5e0
Alex Fuller <boberfly@gmail.com>
2017-09-17 15:55:00 -0700

    Updating libtiff to 4.0.8

--- winbuild/buildTIFF.bat
+++ winbuild/buildTIFF.bat
@@ -1,4 +1,4 @@
-cd %~dp0%..\tiff-3.8.2
+cd %~dp0%..\tiff-4.0.8
 
 mkdir %BUILD_DIR%\doc\licenses
 copy COPYRIGHT %BUILD_DIR%\doc\licenses\libtiff
@@ -7,5 +7,12 @@ nmake /f makefile.vc
 
 copy libtiff\libtiff.lib %BUILD_DIR%\lib
 copy libtiff\*.h %BUILD_DIR%\include
+
+rem mkdir gafferBuild
+rem cd gafferBuild
+rem rm -f CMakeCache.txt
+
+rem cmake -Wno-dev -G %CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE%  -DCMAKE_PREFIX_PATH=%BUILD_DIR% -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% ..
+rem cmake --build . --config %BUILD_TYPE% --target install
 
 cd %ROOT_DIR%
