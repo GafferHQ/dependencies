7fb1f852c27cedbb84065288fd17f75abd0b7418
Alex Fuller <boberfly@gmail.com>
2017-09-17 15:11:22 -0700

    TBB CMake build scripts from https://github.com/wjakob/tbb

--- winbuild/buildTBB.bat
+++ winbuild/buildTBB.bat
@@ -1,19 +1,14 @@
-
-cd %~dp0%..\tbb43_20150611oss
+cd %~dp0%..\tbb-4.4.6
 
 mkdir %BUILD_DIR%\doc\licenses
 copy COPYING %BUILD_DIR%\doc\licenses\tbb
 
-rmdir /s /q build\vs2013\intel64
-msbuild build\vs2013\makefile.sln /t:Build /p:Configuration="Release-MT" /p:Platform=x64
+mkdir gafferBuild
+cd gafferBuild
+rm -f CMakeCache.txt
 
-mkdir %BUILD_DIR%\include\tbb
-xcopy /E /Q /Y include\tbb %BUILD_DIR%\include\tbb
-copy build\vs2013\intel64\Release-MT\tbb.dll %BUILD_DIR%\lib
-copy build\vs2013\intel64\Release-MT\tbbmalloc.dll %BUILD_DIR%\lib
-copy build\vs2013\intel64\Release-MT\tbbmalloc_proxy.dll %BUILD_DIR%\lib
-copy build\vs2013\intel64\Release-MT\tbb.lib %BUILD_DIR%\lib
-copy build\vs2013\intel64\Release-MT\tbbmalloc.lib %BUILD_DIR%\lib
-copy build\vs2013\intel64\Release-MT\tbbmalloc_proxy.lib %BUILD_DIR%\lib
+cmake -Wno-dev -G %CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE%  -DCMAKE_PREFIX_PATH=%BUILD_DIR% -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% -DTBB_BUILD_TESTS=OFF ..
+cmake --build . --config %BUILD_TYPE% --target install
+
 
 cd %ROOT_DIR%
