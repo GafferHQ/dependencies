0c55fab52946d1531b48a246c5b024bb9fa3261a
Alex Fuller <boberfly@gmail.com>
2017-12-27 12:12:15 -0800

    Updates to build scripts on windows

--- winbuild/buildBoost.bat
+++ winbuild/buildBoost.bat
@@ -3,11 +3,16 @@ cd %~dp0%..\boost_1_61_0
 mkdir %BUILD_DIR%\doc\licenses
 copy LICENSE_1_0.txt %BUILD_DIR%\doc\licenses\boost
 
-rem give a helping hand to find the python headers, since the bootstrap below doesn't
-rem always seem to get it right.
-set CPLUS_INCLUDE_PATH=%BUILD_DIR%\\include\\python2.7;%BUILD_DIR%\\include
+set PYTHONPATH=%BUILD_DIR%;%BUILD_DIR%\bin;%BUILD_DIR%\lib64;%BUILD_DIR%\lib
+set SAVEPATH=%PATH%
+set PATH=%BUILD_DIR%\bin;%PATH%
 
-call bootstrap.bat --prefix=%BUILD_DIR% --with-python=%BUILD_DIR%\bin\python --with-python-root=%BUILD_DIR% --without-libraries=log
-b2 --prefix=%BUILD_DIR% --toolset=%BOOST_MSVC_VERSION% architecture=x86 address-model=64 --build-type=complete variant=release link=shared threading=multi -s ZLIB_SOURCE=%ROOT_DIR%\..\zlib-1.2.11 -s ZLIB_INCLUDE=%BUILD_DIR%\include -s ZLIB_LIBPATH=%BUILD_DIR%\lib -s ZLIB_BINARY=zlib install
+call bootstrap.bat --prefix=%BUILD_DIR% --with-python=%BUILD_DIR% --with-python-root=%BUILD_DIR% --without-libraries=log
+set "BUILD_DIR=%BUILD_DIR:\=\\%"
+echo using python ^: ^2.7 : %BUILD_DIR% ^: %BUILD_DIR%\\include ^: %BUILD_DIR%\\lib ^: ^<address-model^>64 ^; >> project-config.jam
+rem b2 --prefix=%BUILD_DIR% --toolset=%BOOST_MSVC_VERSION% architecture=x86 address-model=64 --build-type=complete variant=release link=shared threading=multi -s ZLIB_SOURCE=%ROOT_DIR%\..\zlib-1.2.11 -s ZLIB_INCLUDE=%BUILD_DIR%\include -s ZLIB_LIBPATH=%BUILD_DIR%\lib -s ZLIB_BINARY=zlib install
+set "BUILD_DIR=%BUILD_DIR:\\=\%"
 
 cd %ROOT_DIR%
+
+set PATH=%SAVEPATH%
--- winbuild/buildPySide.bat
+++ winbuild/buildPySide.bat
@@ -1,5 +1,12 @@
 cd %~dp0%..\pyside-setup-6d8dee0
 
+rem set SAVEPATH=%PATH%
+rem set PATH=%BUILD_DIR%\bin;%PATH%
+rem set PYTHONHOME=%BUILD_DIR%
+rem set PYTHONPATH=%BUILD_DIR%\python;%PYTHONPATH%
+
 python setup.py --ignore-git --qmake=%BUILD_DIR%\bin\qmake.exe --openssl=%BUILD_DIR%\lib --cmake="C:\Program Files\CMake\bin\cmake.exe" install
+
+rem set PATH=%SAVEPATH%
 
 cd %ROOT_DIR%
