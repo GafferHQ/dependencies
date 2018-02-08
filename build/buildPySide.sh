da9bd7311a5ed8b697a504e4cc09eb1b6ad64e12
John Haddon <thehaddonyoof@gmail.com>
2018-02-08 15:57:49 +0000

    PySide : Build in parallel

--- build/buildPySide.sh
+++ build/buildPySide.sh
@@ -8,4 +8,7 @@ export LD_LIBRARY_PATH=$BUILD_DIR/lib
 export PATH=$BUILD_DIR/bin:$PATH
 export MACOSX_DEPLOYMENT_TARGET=10.9
 
-python setup.py --ignore-git --osx-use-libc++ install
+# If you need to hack on the PySide source itself, you might like to
+# know that you can use the `--reuse-build` argument to stop `setup.py`
+# from rebuilding the entire goddam thing each time.
+python setup.py --ignore-git --jobs `getconf _NPROCESSORS_ONLN` --osx-use-libc++ install
