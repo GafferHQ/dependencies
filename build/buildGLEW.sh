b9ae4a196a22260d258a0f334ef7ba0fd3c24fab
Alex Fuller <boberfly@gmail.com>
2018-02-09 23:39:44 -0800

    GLEW-2.1.0 upgrade

--- build/buildGLEW.sh
+++ build/buildGLEW.sh
@@ -2,10 +2,25 @@
 
 set -e
 
-cd `dirname $0`/../glew-1.7.0
+GLEW_VERSION="2.1.0"
+ARCHIVE_NAME="glew-$GLEW_VERSION.tgz"
+ARCHIVE_ROOT_DIR="glew-$GLEW_VERSION"
+
+WORKING_DIR=`dirname $0`/../working/glew
+
+rm -rf $WORKING_DIR
+mkdir -p $WORKING_DIR
+cp `dirname $0`/../archives/$ARCHIVE_NAME  $WORKING_DIR
+
+pushd $WORKING_DIR
+
+tar xvf ${ARCHIVE_NAME}
+cd $ARCHIVE_ROOT_DIR
 
 mkdir -p $BUILD_DIR/doc/licenses
 cp LICENSE.txt $BUILD_DIR/doc/licenses/glew
 
 mkdir -p $BUILD_DIR/lib64/pkgconfig
 make clean && make -j `getconf _NPROCESSORS_ONLN` install GLEW_DEST=$BUILD_DIR LIBDIR=$BUILD_DIR/lib
+
+popd
