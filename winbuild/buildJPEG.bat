f5c8964e25d18f88318d77211351ae9d51b1cd98
Eric Mehl <eric@thinkhypothetical.com>
2018-03-16 10:54:12 -0400

    have libJPEG use out of source build like other dependencies

--- winbuild/buildJPEG.bat
+++ winbuild/buildJPEG.bat
@@ -7,7 +7,7 @@ mkdir gafferBuild
 cd gafferBuild
 del /f CMakeCache.txt
 
-cmake -Wno-dev -G %CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DWITH_SIMD=OFF -DCMAKE_INSTALL_PREFIX=%BUILD_DIR%
+cmake -Wno-dev -G %CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DWITH_SIMD=OFF -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% ..
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 cmake --build . --config %BUILD_TYPE% --target install
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
