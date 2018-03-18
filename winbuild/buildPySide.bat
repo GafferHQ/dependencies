cd91a98d03f64e2933a623f648cd47e66de3584e
Eric Mehl <eric@thinkhypothetical.com>
2018-03-18 13:25:31 -0400

    fix Pyside confusion with --ignore-git if VERSION is set in the environment

--- winbuild/buildPySide.bat
+++ winbuild/buildPySide.bat
@@ -4,10 +4,15 @@ set SAVEPATH=%PATH%
 set PATH=%BUILD_DIR%\bin;%PATH%
 set PYTHONHOME=%BUILD_DIR%
 set PYTHONPATH=%BUILD_DIR%\python;%PYTHONPATH%
+rem Pyside will grab "version" from the environment if it exists and get
+rem confused with the --ignore-git flag, so unset VERSION temporarily
+set OLDVERSION=%VERSION%
+set VERSION=
 
 python setup.py --ignore-git --qmake=%BUILD_DIR%\bin\qmake.exe --openssl=%BUILD_DIR%\lib --cmake="C:\Program Files\CMake\bin\cmake.exe" install
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 
 set PATH=%SAVEPATH%
+set VERSION=%OLDVERSION%
 
 cd %ROOT_DIR%
