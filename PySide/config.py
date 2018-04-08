43b757cff7bd1b3c07691cb83cc4af020c7f40e6
Eric Mehl <eric@thinkhypothetical.com>
2018-04-08 15:09:30 -0400

    PySide : add Windows configuration

--- PySide/config.py
+++ PySide/config.py
@@ -25,6 +25,14 @@
 
 	],
 
+	"manifest" : [
+
+		"include/PySide2",
+		"lib/python{pythonVersion}/site-packages/PySide2",
+		"lib/python{pythonVersion}/site-packages/pyside2uic",
+
+	],
+
 	"platform:linux" : {
 
 		"environment" : {
@@ -48,6 +56,48 @@
 			( "{buildDir}/bin/pyside6-uic", "../lib/Python.framework/Versions/Current/bin/pyside6-uic" ),
 
 		]
+
+	},
+
+	"platform:windows" : {
+
+		"manifest" : [
+
+			"include/PySide2",
+			"python/PySide2",
+			"python/pyside2uic.exe",
+			"python/shiboken2",
+			"python/shiboken2_generator",
+
+		],
+
+		"environment" : {
+
+			"PATH" : "{buildDir}\\bin;{buildDir}\\lib;%PATH%",
+			"LLVM_INSTALL_DIR" : "{buildDir}",
+			"VERSION" : "",	# PySide will pull in VERSION from the environment if it exists and cause a failure because the --ignore-git conflicts with VERSION
+
+		},
+
+		"commands" : [
+			"xcopy /s /e /h /y /i %ROOT_DIR%\\PySide\\working\\pyside-setup-opensource-src-6.5.6 %ROOT_DIR%\\PySide\\working\\p",	# Shorten overall paths to avoid Windows command character limit
+			"cd ..\\p && python setup.py install"
+				" --ignore-git"
+				" --openssl={buildDir}\\bin"
+				" --parallel {jobs}"
+				" --qtpaths={buildDir}/bin/qtpaths6.exe"
+				" install",
+		],
+
+		"postMovePaths" : {
+
+			# "{buildDir}/lib/site-packages/PySide2/pyside2-uic.exe" : "{buildDir}/bin",
+			"{buildDir}/lib/site-packages/PySide2/uic.exe" : "{buildDir}/bin",
+			"{buildDir}/lib/site-packages/PySide2" : "{buildDir}/python",
+			"{buildDir}/lib/site-packages/shiboken2" : "{buildDir}/python",
+			"{buildDir}/lib/site-packages/shiboken2_generator" : "{buildDir}/python",
+
+		},
 
 	},
 
