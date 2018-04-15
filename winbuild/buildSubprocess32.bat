a9211f54857377941803a1fd729b5e77c5c54212
Eric Mehl <eric@thinkhypothetical.com>
2018-04-15 15:46:54 -0400

    fix errors in subprocess32.py build

--- winbuild/buildSubprocess32.bat
+++ winbuild/buildSubprocess32.bat
@@ -1,8 +1,8 @@
-cd %~dp0%..\subprocess32-3.2.6
+cd %ROOT_DIR%\..\subprocess32-3.2.6
 
 copy subprocess32.py %BUILD_DIR%\lib64\site-packages\subprocess32.py
-move %BUILD_DIR%\lib\python2.7\site-packages\iex.pyd %BUILD_DIR%\python
+mkdir %BUILD_DIR%\python
 copy subprocess32.py %BUILD_DIR%\python\subprocess32.py
-move %BUILD_DIR%\lib\python2.7\site-packages\iex.pyd %BUILD_DIR%\python
+if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 
 cd %ROOT_DIR%
