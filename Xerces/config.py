dd72615d38b0468c7cec45b543a2fa1c7ca5968d
Eric Mehl <eric@thinkhypothetical.com>
2018-05-02 10:56:55 -0400

    add Xerces build

--- Xerces/config.py
+++ Xerces/config.py
@@ -13,11 +13,29 @@
 		"mkdir gafferBuild",
 		"cd gafferBuild &&"
 		 	" cmake"
-		 	" -G $CMAKE_GENERATOR"
-		 	" -D CMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"
-		 	" -D CMAKE_INSTALL_PREFIX=$BUILD_DIR"
+		 	" -G {cmakeGenerator}"
+		 	" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+		 	" -D CMAKE_INSTALL_PREFIX={buildDir}"
 		 	" ..",
-		"cd gafferBuild && cmake --build . --config $CMAKE_BUILD_TYPE --target install -- -j $NUM_PROCESSORS"
+		"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild &&"
+			 	" cmake"
+			 	" -G {cmakeGenerator}"
+			 	" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+			 	" -D CMAKE_INSTALL_PREFIX={buildDir}"
+			 	" -D BUILD_SHARED_LIBS=OFF"
+			 	" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"	
+
+		]
+
+	}
 
 }
