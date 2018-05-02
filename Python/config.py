13861432c52db73a63a62e13383e8397bec8e951
Eric Mehl <eric@thinkhypothetical.com>
2018-05-02 10:55:02 -0400

    add Python build

--- Python/config.py
+++ Python/config.py
@@ -40,4 +40,36 @@
 
 	},
 
+	"platform:windows" : {
+
+		"downloads" : [
+
+			"https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz",
+			"https://github.com/python-cmake-buildsystem/python-cmake-buildsystem/archive/master.tar.gz",
+
+		],
+
+		"commands" : [
+			"move ..\\python-cmake-buildsystem-master .\\python-cmake-buildsystem",
+
+			"mkdir gafferBuild",
+
+			"cmake"
+				" -Wno-dev"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D PYTHON_VERSION=2.7.15"
+				" -D DOWNLOAD_SOURCES=OFF"
+				" -D BUILD_LIBPYTHON_SHARED=ON"
+				" -D Py_UNICODE_SIZE=4"
+				" -D USE_LIB64=ON"
+				" -D INSTALL_TEST=OFF"
+				" python-cmake-buildsystem",
+			"cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+			"copy {buildDir}\\libs\\*.lib {buildDir}\\lib",
+		]
+
+	}
+
 }
