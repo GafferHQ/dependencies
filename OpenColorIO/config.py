b4461583cc37b6666364aa6fc33e385eefeaf73d
Eric Mehl <eric@thinkhypothetical.com>
2018-05-02 10:52:55 -0400

    OpenColorIO : add Windows configuration

--- OpenColorIO/config.py
+++ OpenColorIO/config.py
@@ -54,10 +54,58 @@
 	"manifest" : [
 
 		"include/OpenColorIO",
-		"lib/libOpenColorIO*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}OpenColorIO*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}OpenColorIO*.lib",
 		"openColorIO",
 		"python/PyOpenColorIO*",
 
 	],
 
+	"platform:windows" : {
+
+		"variables" : {
+
+			"cmakeBuildType" : "Release",
+			"cmakeGenerator": "\"Visual Studio 15 2017 Win64\"",
+
+		},
+
+		"environment" : {
+
+			"PATH" : "{buildDir}\\bin;%PATH%;%ROOT_DIR%\\winbuild\\patch\\bin",
+
+		},
+
+		"commands" : [
+
+			#OCIO is particular about needing all slashes to be forward slashes
+			"mkdir build",
+			"cd build && cmake"
+				" -Wno-dev"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_CXX_STANDARD={c++Standard}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+                " -D CMAKE_INSTALL_LIBDIR={buildDir}/lib"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D Python_ROOT_DIR={buildDir}"
+				" -D Python_FIND_STRATEGY=LOCATION"
+                " -D pystring_INCLUDE_DIR={buildDir}/include"
+				" -D BUILD_SHARED_LIBS=ON"
+                " -D OCIO_INSTALL_EXT_PACKAGES=NONE"
+				" -D OCIO_BUILD_APPS=OFF"
+				" -D OCIO_BUILD_NUKE=OFF"
+				" -D OCIO_BUILD_TESTS=OFF"
+				" -D OCIO_BUILD_GPU_TESTS=OFF"
+				" -D OCIO_PYTHON_VERSION={pythonVersion}"
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install",
+            "if not exist \"{buildDir}\\openColorIO\" mkdir {buildDir}\\openColorIO",
+			"if not exist \"{buildDir}\\openColorIO\\luts\" mkdir {buildDir}\\openColorIO\\luts",
+			"copy ..\\OpenColorIO-Configs-1.0_r2\\nuke-default\\config.ocio {buildDir}\\openColorIO",
+			"xcopy /s /e /h /y /i ..\\OpenColorIO-Configs-1.0_r2\\nuke-default\\luts {buildDir}\\openColorIO\\luts",
+			"copy {buildDir}\\bin\\{libraryPrefix}OpenColorIO*{sharedLibraryExtension}* {buildDir}\\lib\\",
+		],
+
+	},
 }
