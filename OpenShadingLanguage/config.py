59e17ca0430c785e771af458cc82d37683db6fd6
Eric Mehl <eric@thinkhypothetical.com>
2018-05-02 10:53:46 -0400

    OpenShadingLanguage : add Windows configuration

--- OpenShadingLanguage/config.py
+++ OpenShadingLanguage/config.py
@@ -34,4 +34,54 @@
 
 	],
 
+	"platform:windows" : {
+
+		"variables" : {
+
+			"version" : "1.9.9",
+
+		},
+
+		"environment" : {
+			"PATH" : "%PATH%;{buildDir}\\lib;{buildDir}\\bin;%ROOT_DIR%\\winbuild\\FlexBison\\bin;%ROOT_DIR%\\OpenShadingLanguage\\working\\OpenShadingLanguage-Release-{version}\\gafferBuild\\src\\liboslcomp",
+
+		},
+
+		"commands" : [
+			"mkdir gafferBuild",
+			"cd gafferBuild &&"
+			 	" cmake"
+			 	" -Wno-dev -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D BUILDSTATIC=OFF"
+				" -D USE_OIIO_STATIC=OFF"
+				" -D OSL_BUILD_PLUGINS=OFF"
+				" -D ENABLERTTI=1"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D STOP_ON_WARNING=0"
+				" -D LLVM_DIRECTORY={buildDir}"
+				" -D USE_LLVM_BITCODE=OFF"
+				" -D OSL_BUILD_TESTS=OFF"
+				" -D BOOST_ROOT={buildDir}"
+				" -D Boost_USE_STATIC_LIBS=OFF"
+				" -D LLVM_STATIC=ON"
+				" -D OPENEXR_INCLUDE_PATH={buildDir}\\include"
+				" -D OPENEXR_IMATH_LIBRARY={buildDir}\\lib\\Imath-2_2.lib"
+				" -D OPENEXR_ILMIMF_LIBRARY={buildDir}\\lib\\IlmImf-2_2.lib"
+				" -D OPENEXR_IEX_LIBRARY={buildDir}\\lib\\Iex-2_2.lib"
+				" -D OPENEXR_ILMTHREAD_LIBRARY={buildDir}\\lib\\IlmThread-2_2.lib"
+				" -D OPENIMAGEIOHOME={buildDir}"
+				" -D ZLIB_INCLUDE_DIR={buildDir}\\include"
+				" -D ZLIB_LIBRARY={buildDir}\\lib\\zlib.lib"
+				" -D EXTRA_CPP_ARGS=\" /D TINYFORMAT_ALLOW_WCHAR_STRINGS\""
+				" -D FLEX_EXECUTABLE=flex"
+				" -D BISON_EXECUTABLE=bison"
+			 	" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"
+		
+		],
+
+	}
+
 }
