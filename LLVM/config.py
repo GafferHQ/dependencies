d39b279efbbab7aa268f5760f69a462d14cc7953
Eric Mehl <eric@thinkhypothetical.com>
2018-05-02 10:52:36 -0400

    LLVM : add Windows configuration

--- LLVM/config.py
+++ LLVM/config.py
@@ -49,4 +49,37 @@
 
 	},
 
+	"platform:windows" : {
+
+		"variables" : {
+
+			"cmakeGenerator" : "\"Ninja\"",
+
+		},
+
+		"commands" : [
+
+			"move ..\\clang* tools\\clang",
+            "patch -p1 < ..\\..\\patches\\windows\\noAST.patch.txt",
+            "move ..\\cmake* ..\\cmake",
+			"mkdir build",
+			"cd build &&"
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_BUILD_TYPE=Release"
+				" -D BUILD_SHARED_LIBS=OFF"
+				" -D LLVM_REQUIRES_RTTI=ON"
+				" -D LLVM_ENABLE_LIBXML2=OFF"
+				" -D LLVM_ENABLE_ZSTD=OFF"
+				" -D LLVM_INCLUDE_BENCHMARKS=OFF"
+				" -D LLVM_INCLUDE_TESTS=OFF"
+				" -D LLVM_TARGETS_TO_BUILD='Native;NVPTX'"
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install",
+
+		],
+
+	}
+
 }
