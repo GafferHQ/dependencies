48e4a794eb05c7c284a107066551f6024ab827d8
John Haddon <thehaddonyoof@gmail.com>
2018-05-03 12:34:04 +0100

    PySide : Prepend to library path rather than replace it
    This is needed for platforms like CentOS, where software collections set up the library path when they are enabled. The current GafferHQ/build setup currently uses just such a system.

--- build/buildPySide.sh
+++ build/buildPySide.sh
@@ -4,7 +4,7 @@ set -e
 
 cd `dirname $0`/../pyside-setup-6d8dee0
 
-export LD_LIBRARY_PATH=$BUILD_DIR/lib
+export LD_LIBRARY_PATH=$BUILD_DIR/lib${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH:-}
 export PATH=$BUILD_DIR/bin:$PATH
 export MACOSX_DEPLOYMENT_TARGET=10.9
 
