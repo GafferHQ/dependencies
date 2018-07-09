d3ad2bc098cbd5cc3bb9fea67aed10cac119ce06
Eric Mehl <eric@thinkhypothetical.com>
2018-07-09 17:50:25 -0400

    GLEW : add Windows configuration

--- GLEW/config.py
+++ GLEW/config.py
@@ -19,8 +19,24 @@
 	"manifest" : [
 
 		"include/GL",
-		"lib/libGLEW*{sharedLibraryExtension}*",
+		"{sharedLibraryDir}/{libraryPrefix}GLEW*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}GLEW*.lib",
 
 	],
+	"platform:windows" : {
+
+		"commands" : [
+
+			"if not exist \"build\" mkdir build",
+			"cd build &&"
+				" cmake"
+				" -Wno-dev"
+				" -G {cmakeGenerator}"
+				" -DCMAKE_INSTALL_PREFIX={buildDir}"
+				" ..\\build\\cmake",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+		]
+
+	}
 
 }
