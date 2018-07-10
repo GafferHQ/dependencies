58704c9bacfe6ce2fc40b05bd74de45172a2a4dd
Eric Mehl <eric@thinkhypothetical.com>
2018-07-10 10:25:42 -0400

    OpenImageIO : add Windows configuration

--- OpenImageIO/config.py
+++ OpenImageIO/config.py
@@ -20,7 +20,45 @@
 			" ..",
 		"cd gafferBuild && make install -j {jobs} VERBOSE=1",
 		"cp {buildDir}/share/doc/OpenImageIO/openimageio.pdf {buildDir}/doc",
-
+		
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild &&"
+				" cmake"
+			 	" -G {cmakeGenerator}"
+			 	" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+			 	" -D CMAKE_INSTALL_PREFIX={buildDir}"
+			 	" -D CMAKE_PREFIX_PATH={buildDir}"
+			 	" -D USE_FFMPEG=OFF"
+			 	" -D USE_QT=OFF"
+			 	" -D USEPYTHON=OFF"
+			 	" -D BUILDSTATIC=OFF"
+			 	" -D OPENEXR_INCLUDE_PATH={buildDir}\\include"
+			 	" -D OPENEXR_IMATH_LIBRARY={buildDir}\\lib\\Imath-2_2.lib"
+			 	" -D OPENEXR_ILMIMF_LIBRARY={buildDir}\\lib\\IlmImf-2_2.lib"
+				" -D OPENEXR_IEX_LIBRARY={buildDir}\\lib\\Iex-2_2.lib"
+				" -D OPENEXR_ILMTHREAD_LIBRARY={buildDir}\\lib\\IlmThread-2_2.lib"
+				" -D ZLIB_INCLUDE_DIR={buildDir}\\include"
+				" -D ZLIB_LIBRARY={buildDir}\\lib\\zlib.lib"
+				" -D PNG_PNG_INCLUDE_DIR={buildDir}\\include"
+				" -D PNG_LIBRARY={buildDir}\\lib\\libpng16.lib"
+				" -D JPEG_INCLUDE_DIR={buildDir}\\include"
+				" -D JPEG_LIBRARY={buildDir}\\lib\\jpeg.lib"
+				" -D TIFF_INCLUDE_DIR={buildDir}\\include"
+				" -D TIFF_LIBRARY={buildDir}\\lib\\libtiff.lib"
+				" -D PYTHON_INCLUDE_DIR={buildDir}\\include\\python2.7"
+				" -D PYTHON_LIBRARY={buildDir}\\lib\\python27.lib"
+				" -D OCIO_LIBRARY_PATH={buildDir}\\lib\\OpenColorIO.lib"
+			 	" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+
+		]
+
+	}
 
 }
