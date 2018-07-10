bd2fef846bfb572ce0efc6ae40533af4601dc389
Eric Mehl <eric@thinkhypothetical.com>
2018-07-10 10:25:42 -0400

    OpenImageIO : add Windows configuration

--- OpenImageIO/config.py
+++ OpenImageIO/config.py
@@ -58,15 +58,80 @@
 
 	"manifest" : [
 
-		"bin/maketx",
-		"bin/oiiotool",
+		"bin/maketx{executableExtension}",
+		"bin/oiiotool{executableExtension}",
 
 		"include/OpenImageIO",
-		"lib/libOpenImageIO*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}OpenImageIO*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}OpenImageIO*.lib",
 
 		"doc/openimageio.pdf",
 
 	],
+	"platform:windows" : {
+
+		"environment" : {
+
+			"PATH" : "{buildDir}/bin;{buildDir}/lib;%PATH%",
+
+		},
+        
+		"manifest" : [
+
+			"bin/maketx{executableExtension}",
+			"bin/oiiotool{executableExtension}",
+
+			"include/OpenImageIO",
+			"lib/{libraryPrefix}OpenImageIO*{sharedLibraryExtension}*",
+			"lib/{libraryPrefix}OpenImageIO*.lib",
+            "python/OpenImageIO",
+
+			"doc/openimageio.pdf",
+
+		],
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild &&"
+				" cmake"
+			 	" -G {cmakeGenerator}"
+				" -D CMAKE_CXX_STANDARD={c++Standard}"
+			 	" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+			 	" -D CMAKE_INSTALL_PREFIX={buildDir}"
+			 	" -D CMAKE_PREFIX_PATH={buildDir}"
+			 	" -D USE_FFMPEG=NO"
+			 	" -D USE_QT=NO"
+			 	" -D USE_PYTHON=YES"
+			 	" -D BUILDSTATIC=NO"
+				" -D BOOST_ROOT={buildDir}"
+				" -D OIIO_BUILD_TESTS=NO"
+				" -D OIIO_DOWNLOAD_MISSING_TESTDATA=NO"
+			 	" -D OPENEXR_INCLUDE_PATH={buildDir}/include"
+			 	" -D OPENEXR_IMATH_LIBRARY={buildDir}/lib/Imath.lib"
+			 	" -D OPENEXR_ILMIMF_LIBRARY={buildDir}/lib/IlmImf.lib"
+				" -D OPENEXR_IEX_LIBRARY={buildDir}/lib/Iex.lib"
+				" -D OPENEXR_ILMTHREAD_LIBRARY={buildDir}/lib/IlmThread.lib"
+				" -D ZLIB_INCLUDE_DIR={buildDir}/include"
+				" -D ZLIB_LIBRARY={buildDir}/lib/zlib.lib"
+				" -D PNG_PNG_INCLUDE_DIR={buildDir}/include"
+				" -D PNG_LIBRARY={buildDir}/lib/libpng16.lib"
+				" -D JPEG_INCLUDE_DIR={buildDir}/include"
+				" -D JPEG_LIBRARY={buildDir}/lib/jpeg.lib"
+				" -D TIFF_INCLUDE_DIR={buildDir}/include"
+				" -D TIFF_LIBRARY={buildDir}/lib/libtiff.lib"
+				" -D PYTHON_VERSION={pythonMajorVersion}"
+				" -D Python_ROOT_DIR={buildDir}"
+				" -D Python_FIND_STRATEGY=LOCATION"
+				" -D OCIO_LIBRARY_PATH={buildDir}/lib/OpenColorIO.lib"
+				" -D USE_SIMD=sse4.2"
+			 	" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+			"copy {buildDir}\\bin\\{libraryPrefix}OpenImageIO*{sharedLibraryExtension}* {buildDir}\\lib\\",
+            "move {buildDir}\\lib\\python{pythonVersion}\\site-packages\\OpenImageIO {buildDir}\\python\\OpenImageIO",
+		]
+
+	}
 
 	"platform:macos" : {
 
