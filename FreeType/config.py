8fd87184c5571511390370c06581b2761d7963c4
John Haddon <thehaddonyoof@gmail.com>
2018-07-23 13:28:52 +0100

    FreeType : Update to version 2.9.1
    Also move to config-based build

---
+++ FreeType/config.py
@@ -0,0 +1,25 @@
+{
+
+	"downloads" : [
+
+		"https://download.savannah.gnu.org/releases/freetype/freetype-2.9.1.tar.gz",
+
+	],
+
+	"license" : "docs/FTL.TXT",
+
+	"environment" : {
+
+		"LDFLAGS" : "-L{buildDir}/lib",
+
+	},
+
+	"commands" : [
+
+		"./configure --prefix={buildDir} --with-harfbuzz=no",
+		"make -j {jobs}",
+		"make install",
+
+	],
+
+}
