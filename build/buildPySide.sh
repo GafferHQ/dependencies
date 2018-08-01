21389054f96495076086dc9ed7cfd1e66655f08d
John Haddon <thehaddonyoof@gmail.com>
2018-08-01 19:26:35 +0100

    PySide : Don't specify MACOSX_DEPLOYMENT_TARGET
    This was leading to the following build error :
    
    ```
    error: $MACOSX_DEPLOYMENT_TARGET mismatch: now "10.9" but "10.12" during configure
    ```
    
    I suspect this is due to the recent Python upgrade, since PySide seems to use distutils, and that has many references to MACOSX_DEPLOYMENT_TARGET.

--- build/buildPySide.sh
+++ build/buildPySide.sh
@@ -6,7 +6,6 @@ cd `dirname $0`/../pyside-setup-6d8dee0
 
 export LD_LIBRARY_PATH=$BUILD_DIR/lib${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH:-}
 export PATH=$BUILD_DIR/bin:$PATH
-export MACOSX_DEPLOYMENT_TARGET=10.9
 
 # If you need to hack on the PySide source itself, you might like to
 # know that you can use the `--reuse-build` argument to stop `setup.py`
