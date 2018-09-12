ba9db485e8c06887a9ea5d357095712e5aa54958
John Haddon <thehaddonyoof@gmail.com>
2018-09-12 19:43:53 +0100

    PNG : Revert to previous version and build commands

--- LibPNG/config.py
+++ LibPNG/config.py
@@ -2,7 +2,7 @@
 
 	"downloads" : [
 
-		"https://download.sourceforge.net/libpng/libpng-1.6.35.tar.gz"
+		"https://download.sourceforge.net/libpng/libpng-1.6.3.tar.gz"
 
 	],
 
@@ -10,14 +10,9 @@
 
 	"commands" : [
 
-		"mkdir gafferBuild",
-		"cd gafferBuild &&"
-			" cmake"
-			" -G \"Unix Makefiles\""
-			" -D CMAKE_INSTALL_PREFIX={buildDir}"
-			" -D CMAKE_PREFIX_PATH={buildDir}"
-			" ..",
-		"cd gafferBuild && make install -j {jobs} VERBOSE=1"
+		"./configure --prefix={buildDir}",
+		"make -j {jobs}",
+		"make install",
 
 	],
 
