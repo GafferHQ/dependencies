dbe8966bec33a88d59787377964576a849b3d2fa
Eric Mehl <eric@thinkhypothetical.com>
2018-12-18 11:22:58 -0500

    Zlib : add Windows configuration

---
+++ Zlib/config.py
@@ -0,0 +1,48 @@
+{
+
+	"downloads" : [
+
+		"https://github.com/madler/zlib/archive/v1.2.11.tar.gz"
+
+	],
+
+	"license" : "LICENSE",
+
+	"commands" : [
+
+		"mkdir gafferBuild",
+
+		"cd gafferBuild &&"
+			" cmake"
+			" -G {cmakeGenerator}"
+			" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+			" -D CMAKE_INSTALL_PREFIX={buildDir}"
+			" ..",
+
+		"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"
+
+	],
+
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+
+			"cd gafferBuild && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" ..",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+			# for some reason Zlib building doesn't put zconf.h in the main source directory alongside zlib.h
+			# manually copy it there so Boost can find it
+			"cd gafferBuild && copy zconf.h ..",
+
+		]
+
+	}
+
+}
