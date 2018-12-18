bc33b25a914d08d3045ed7f33d8ea1adc1a9d259
Eric Mehl <eric@thinkhypothetical.com>
2018-12-18 12:46:03 -0500

    CMark : add Windows configuration

--- CMark/config.py
+++ CMark/config.py
@@ -16,4 +16,22 @@
 
 	],
 
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" ..",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+
+		],
+
+	}
+	
 }
