f51e564349f56b8bc269c875e14ccee11d4e4b6e
Eric Mehl <eric@thinkhypothetical.com>
2018-12-18 12:47:13 -0500

    LibJPEG-Turbo : add Windows configuration

--- LibJPEG-Turbo/config.py
+++ LibJPEG-Turbo/config.py
@@ -24,8 +24,30 @@
 		"include/jmorecfg.h",
 		"include/jpeglib.h",
 
-		"lib/libjpeg*{sharedLibraryExtension}*",
-
+		"lib/libjpeg*{sharedLibraryExtension}*",	# Linux / Mac OS
+		"lib/libjpeg.lib",
+        "lib/jpeg.lib",
+		"lib/jpeg62{sharedLibraryExtension}",	# Windows only
+		"lib/turbojpeg{sharedLibraryExtension}",	# Windows only
 	],
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D WITH_SIMD=OFF"
+				" ..",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+			"copy {buildDir}\\bin\\jpeg62{sharedLibraryExtension} {buildDir}\\lib\\",
+			"copy {buildDir}\\bin\\turbojpeg{sharedLibraryExtension} {buildDir}\\lib\\",
+		],
+
+	},
 
 }
