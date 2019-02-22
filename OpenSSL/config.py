ab78bcb72348d8765e51d68e8ddc6eef27902ef1
Alex Fuller <boberfly@gmail.com>
2019-02-22 00:37:29 -0800

    Windows OpenSSL + patches

--- OpenSSL/config.py
+++ OpenSSL/config.py
@@ -26,4 +26,26 @@
 
 	},
 
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D BUILD_OBJECT_LIBRARY_ONLY=ON"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" ..",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} -- -j {jobs}",
+			"cd gafferBuild && copy crypto\\crypto.lib {buildDir}\\lib",
+			"cd gafferBuild && copy ssl\\ssl.lib {buildDir}\\lib",
+			"cd gafferBuild && xcopy /E /Q /Y include {buildDir}\\include",
+
+		],
+
+	},
+
 }
