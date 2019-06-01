d2607e80d73673d9554f84c519fd8db28a79bec2
Sachin Shrestha <noizfactory@gmail.com>
2019-06-01 09:37:41 +0530

    Added support for reading usd files with alembic files references. Discussed in the thread - https://groups.google.com/forum/#!topic/gaffer-dev/bkCJkOFBfh4

--- USD/config.py
+++ USD/config.py
@@ -17,6 +17,10 @@
 			" -D Boost_NO_BOOST_CMAKE=TRUE"
 			" -D PXR_BUILD_IMAGING=FALSE"
 			" -D PXR_BUILD_TESTS=FALSE"
+			" -D PXR_BUILD_ALEMBIC_PLUGIN=TRUE"
+			" -D PXR_ENABLE_HDF5_SUPPORT=FALSE"
+			" -D ALEMBIC_DIR={buildDir}/lib"
+			" -D OPENEXR_LOCATION={buildDir}/lib"
 			# Needed to prevent CMake picking up system python libraries on Mac.
 			" -D CMAKE_FRAMEWORK_PATH={buildDir}/lib/Python.framework/Versions/2.7/lib"
 			" ."
