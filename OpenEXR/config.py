f6e34173b366f77de1433b406cde95003f0447e1
Eric Mehl <eric@thinkhypothetical.com>
2019-07-24 12:01:42 -0400

    OpenEXR : add Windows configuration

--- OpenEXR/config.py
+++ OpenEXR/config.py
@@ -37,12 +37,54 @@
 
 	"manifest" : [
 
-		"bin/exrheader",
+		"bin/exrheader.*",
 		"include/OpenEXR",
-		"lib/libIex*{sharedLibraryExtension}*",
-		"lib/libIlmThread*{sharedLibraryExtension}*",
-		"lib/libOpenEXR*{sharedLibraryExtension}*",
+		"{sharedLibraryDir}/{libraryPrefix}Iex*{sharedLibraryExtension}*",
+        "lib/{libraryPrefix}Iex*.lib",
+		"{sharedLibraryDir}/{libraryPrefix}IlmThread*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}IlmThread*.lib",
+		"{sharedLibraryDir}/{libraryPrefix}OpenEXR*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}OpenEXR*.lib",
 
 	],
+
+	"platform:windows" : {
+        
+		"environment" : {
+
+			"PATH" : "{buildDir}\\bin;%PATH%",
+
+		},
+
+		"variables" : {
+
+			"cmakeGenerator" : "\"Visual Studio 17 2022\"",
+
+		},
+
+		"dependencies" : [ "Python", "Boost", "Imath", "ZLib" ],
+
+		"commands" : [
+			"mkdir gafferBuild",
+			"cd gafferBuild && cmake"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D OPENEXR_LIB_SUFFIX="
+				" -D ILMBASE_LIB_SUFFIX="
+				" -D PYILMBASE_LIB_SUFFIX="
+				" -D Boost_NO_SYSTEM_PATHS=TRUE"
+				" -D Boost_NO_BOOST_CMAKE=TRUE"
+				" -D BOOST_ROOT={buildDir}"
+				" -D Python3_ROOT_DIR={buildDir}"
+				" -D Python_NO_SYSTEM_PATHS=TRUE"
+				" -D Python3_FIND_STRATEGY=LOCATION"
+				" -D CMAKE_CXX_FLAGS=\"-DBOOST_ALL_NO_LIB\""
+				" -D ZLIB_ROOT={buildDir}"
+				" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install",
+		]
+	},
 
 }
