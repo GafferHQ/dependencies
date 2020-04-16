e22d64e1ff37acfcc9450ec95d76f405fbf3cb1b
John Haddon <thehaddonyoof@gmail.com>
2020-04-16 08:43:27 +0100

    OpenImageIO : Don't build `iv`
    This fails on Mac. We've avoided this problem only by fluke until now, because `iv` requires Qt, and Qt is usually built _after_ OIIO.

--- OpenImageIO/config.py
+++ OpenImageIO/config.py
@@ -20,6 +20,11 @@
 			" -D CMAKE_PREFIX_PATH={buildDir}"
 			" -D USE_FFMPEG=NO"
 			" -D USE_PYTHON=NO"
+			# These next two disable `iv`. This fails to
+			# build on Mac due to OpenGL deprecations, and
+			# we've never packaged it anyway.
+			" -D USE_OPENGL=NO"
+			" -D USE_QT=NO"
 			" ..",
 		"cd gafferBuild && make install -j {jobs} VERBOSE=1",
 		"cp {buildDir}/share/doc/OpenImageIO/openimageio.pdf {buildDir}/doc",
