99c0d5dcb31ab8cf5e6d624b58e26af22ea31ae7
Eric Mehl <eric@thinkhypothetical.com>
2020-05-20 10:57:10 -0400

    Qt : add Windows configuration

--- Qt/config.py
+++ Qt/config.py
@@ -70,14 +70,15 @@
 
 	"manifest" : [
 
-		"bin/moc",
-		"bin/qmake",
-		"bin/rcc",
-		"bin/uic",
+		"bin/moc{executableExtension}",
+		"bin/qmake{executableExtension}",
+		"bin/rcc{executableExtension}",
+		"bin/uic{executableExtension}",
 
 		"include/Qt*",
 
-		"lib/libQt*",
+		"{sharedLibraryDir}/{libraryPrefix}Qt*{sharedLibraryExtension}",
+		"lib/{libraryPrefix}Qt*",
 		"lib/Qt*.framework",
 
 		"mkspecs",
@@ -118,5 +119,75 @@
 		},
 
 	},
+
+	"platform:windows" : {
+
+		"environment" : {
+
+			"PATH" : "{buildDir}\\bin;%PATH%",
+
+		},
+
+		"commands" : [
+
+			# lambda c : shutil.copy( c["variables"]["buildDir"] + "/lib/zlib.lib", c["variables"]["buildDir"] + "/lib/zdll.lib" ),
+			# lambda c : shutil.copy( c["variables"]["buildDir"] + "/lib/libpng16.lib", c["variables"]["buildDir"] + "/lib/libpng.lib" ),
+			# lambda c : shutil.copy( c["variables"]["buildDir"] + "/lib/jpeg.lib", c["variables"]["buildDir"] + "/lib/libjpeg.lib" ),
+			# We need the following DLLs to be available to the Qt build. But if we add `{buildDir}\\bin` to `PATH`, the Qt build
+			# fails due to errors with the clang++ it finds in that directory. Instead of polluting `PATH`, just make them easier to find.
+            # lambda c : shutil.copy( c["variables"]["buildDir"] + "/bin/zlib.dll", os.environ["ROOT_DIR"] + "/Qt/working/qt-everywhere-src-6.5.6/qtbase/bin/zlib.dll" ),
+			# lambda c : shutil.copy( c["variables"]["buildDir"] + "/bin/libpng16.dll", os.environ["ROOT_DIR"] + "/Qt/working/qt-everywhere-src-6.5.6/qtbase/bin/libpng16.dll" ),
+			"call configure.bat"
+				" -prefix {buildDir}"
+				" -cmake-generator Ninja"
+				" -plugindir {buildDir}/qt/plugins"
+				" -release"
+				" -opensource"
+				" -confirm-license"
+				" -opengl desktop"
+				" -no-rpath"
+				" -no-dbus"
+				" -skip qtconnectivity"
+				" -skip qtdatavis3d"
+				" -skip qtdeclarative"
+				" -skip qtgamepad"
+				" -skip qtnetworkauth"
+				" -skip qtremoteobjects"
+				" -skip qtsensors"
+				" -skip qtserialbus"
+				" -skip qtserialport"
+				" -skip qtspeech"
+				" -skip qtwebchannel"
+				" -skip qtwebengine"
+				" -skip qtdoc"
+				" -skip qthttpserver"
+				" -skip qtlocation"
+				" -skip qtlottie"
+				" -skip qtmqtt"
+				" -skip qtopcua"
+				" -skip qtquick3d"
+				" -skip qtquick3dphysics"
+				" -skip qtquickeffectmaker"
+				" -skip qtquicktimeline"
+				" -skip qtvirtualkeyboard"
+				" -skip qtwebsockets"
+				" -skip qtwebview"
+				" -no-libudev"
+				" -no-icu"
+				" -qt-pcre"
+				" -nomake examples"
+				" -nomake tests"
+				" -system-zlib"
+				" -I {buildDir}/include"
+				" -L {buildDir}/lib"
+				" --"
+				" -DCMAKE_CXX_COMPILER=cl.exe"
+				" -DCMAKE_C_COMPILER=cl.exe"
+				" -DCMAKE_ASM_COMPILER=cl.exe",
+			"cmake --build . --parallel",
+			"cmake --install ."
+
+		]
+	}
 
 }
