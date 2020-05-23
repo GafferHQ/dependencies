2f968f0d08a79bc4e3e22379eae0ee4d262669ef
Eric Mehl <eric@thinkhypothetical.com>
2020-05-23 15:33:05 -0400

    LZ4 : add Windows configuration

--- LZ4/config.py
+++ LZ4/config.py
@@ -21,4 +21,24 @@
 
 	],
 
+	"platform:windows" : {
+
+		"manifest" : [
+
+			"lib\\liblz4.dll",
+			"lib\\liblz4.lib",
+
+		],
+
+		"commands" : [
+
+			# From the LZ4 appveyor script
+			"msbuild visual\\VS2017\\lz4.sln /m /verbosity:minimal /property:PlatformToolset=v142 /property:WindowsTargetPlatformVersion=10.0.19041.0 /t:Clean,Build /p:Platform=x64 /p:Configuration=Release",
+			"copy visual\\VS2017\\bin\\x64_Release\\liblz4.dll {buildDir}\\lib",
+			"copy visual\\VS2017\\bin\\x64_Release\\liblz4.lib {buildDir}\\lib",
+
+		],
+
+	},
+
 }
