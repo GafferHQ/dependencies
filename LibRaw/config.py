b3552b551a044ba58cb243fccdaf4efaed5b8e52
Eric Mehl <eric@thinkhypothetical.com>
2020-10-15 16:41:46 -0400

    LibRAW : add Windows configuration

--- LibRaw/config.py
+++ LibRaw/config.py
@@ -18,11 +18,27 @@
 
 	],
 
+	# The "lib" prefix is correct for all platforms including Windows
 	"manifest" : [
 
 		"include/libraw",
-		"lib/libraw*{sharedLibraryExtension}*",
+		"{sharedLibraryDir}/libraw*{sharedLibraryExtension}*",
+		"lib/libraw{staticLibraryExtension}",
 
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+
+			"nmake -f Makefile.msvc",
+			"xcopy /s /e /h /y /i libraw include\\libraw",
+			"copy bin\\libraw.dll lib\\libraw.dll"
+
+		],
+
+	},
+
+	
 
 }
