552ef906bc0b3fce8827331cf148f81a0124d4bf
Eric Mehl <eric@thinkhypothetical.com>
2022-03-09 13:10:32 -0500

    Embree : Add Windows configuration

--- Embree/config.py
+++ Embree/config.py
@@ -40,8 +40,39 @@
 
 		"include/embree4",
 
-		"lib/libembree4*{sharedLibraryExtension}*",
+		"{sharedLibraryDir}/{libraryPrefix}embree4*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}embree4*.lib",
 
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild && cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D CMAKE_INSTALL_LIBDIR={buildDir}/lib"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D TBB_ROOT={buildDir}"
+				" -D EMBREE_STATIC_LIB=OFF"
+				" -D EMBREE_ISPC_SUPPORT=OFF"
+				" -D EMBREE_TUTORIALS=OFF"
+				" -D EMBREE_RAY_MASK=ON"
+				" -D EMBREE_FILTER_FUNCTION=ON"
+				" -D EMBREE_BACKFACE_CULLING=OFF"
+				" -D EMBREE_TASKING_SYSTEM=TBB"
+
+				# " -D CMAKE_INSTALL_INCLUDEDIR={buildDir}/include"
+				# " -D CMAKE_INSTALL_DATAROOTDIR="
+				# " -D CMAKE_INSTALL_DOCDIR=doc"
+				# " -D CMAKE_INSTALL_BINDIR=bin"
+				" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install",
+		]
+
+	},
 
 }
