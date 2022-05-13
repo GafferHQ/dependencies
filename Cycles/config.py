935d6fe1728dd52e33625db694b12c01cfa5ddf2
Eric Mehl <eric@thinkhypothetical.com>
2022-05-13 17:44:54 -0400

    Cycles : Add Windows configuration

--- Cycles/config.py
+++ Cycles/config.py
@@ -47,4 +47,49 @@
 
 	],
 
+	"platform:windows" : {
+	
+		"environment" : {
+
+			"PATH" : "{buildDir}\\bin;%PATH%",
+
+		},
+
+		"commands" : [
+
+			"mkdir build",
+			"cd build &&"
+				" cmake"
+				" -W-nodev -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}/cycles"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D WITH_CYCLES_OPENIMAGEDENOISE=OFF"
+				" -D WITH_CYCLES_PATH_GUIDING=ON"
+				" -D WITH_CYCLES_CUDA_BINARIES=ON"
+				" -D WITH_CYCLES_DEVICE_CUDA=ON"
+				" -D WITH_CYCLES_DEVICE_HIP=OFF"
+				" -D WITH_CYCLES_DEVICE_OPTIX=ON"
+				" -D OPTIX_ROOT_DIR=\"C:/ProgramData/NVIDIA Corporation/Optix SDK 7.7.0\""
+				" -D WITH_CYCLES_HYDRA_RENDER_DELEGATE=OFF"
+				" -D CMAKE_POSITION_INDEPENDENT_CODE=ON"
+				" -D WITH_CYCLES_USD=OFF"
+				" -D TBB_ROOT_DIR={buildDirFwd}"
+				" -D OPENVDB_ROOT_DIR={buildDirFwd}"
+				" -D NANOVDB_INCLUDE_DIR={buildDirFwd}/include"
+				" -D OPENPGL_ROOT_DIR={buildDirFwd}"
+				" -D PLATFORM_BUNDLED_LIBRARY_DIRS={buildDir}\\bin;{buildDir}\\lib;%PATH%"
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install VERBOSE=1 -- -j {jobs}",
+			"if not exist \"{buildDir}\\cycles\\bin\" mkdir {buildDir}\\cycles\\bin",
+			"if not exist \"{buildDir}\\cycles\\include\" mkdir {buildDir}\\cycles\\include",
+			"copy build\\bin\\cycles.* {buildDir}\\cycles\\bin",
+			"xcopy /sehyi build\\lib {buildDir}\\cycles\\lib",
+			"xcopy /sehyi third_party\\atomic\\* {buildDir}\\cycles\\include",
+			"xcopy /shyi src\\*.h {buildDir}\\cycles\\include",
+
+		]
+
+	}
+
 }
