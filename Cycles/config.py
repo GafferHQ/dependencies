8bbf9bf93deb0880537212190f041a61cde357b6
Alex <boberfly@gmail.com>
2022-06-09 19:55:10 -0700

    Cycles : update to 3.2.0
    - Removed the now unneeded oiio.patch as it is fixed upstream
    - Added binpath.patch so that cycles can find its directory structure when moved into the bin directory

--- Cycles/config.py
+++ Cycles/config.py
@@ -2,7 +2,7 @@
 
 	"downloads" : [
 
-		"https://github.com/blender/cycles/archive/refs/tags/v3.1.1.tar.gz",
+		"https://github.com/blender/cycles/archive/refs/tags/v3.2.0.tar.gz",
 
 	],
 
@@ -10,7 +10,7 @@
 
 	"license" : "LICENSE",
 
-	"dependencies" : [ "Boost", "OpenJPEG", "OpenImageIO", "TBB", "Alembic", "Embree", "OpenColorIO", "OpenVDB", "OpenShadingLanguage" ],
+	"dependencies" : [ "Boost", "OpenJPEG", "OpenImageIO", "TBB", "Alembic", "Embree", "OpenColorIO", "OpenVDB", "OpenShadingLanguage", "USD" ],
 
 	"commands" : [
 
@@ -24,13 +24,15 @@
 			" -D WITH_CYCLES_DEVICE_CUDA=OFF"
 			" -D WITH_CYCLES_DEVICE_OPTIX=OFF"
 			" -D CMAKE_POSITION_INDEPENDENT_CODE=ON"
+			" -D PXR_ROOT={buildDir}"
 			" ..",
 		"cd build && make install -j {jobs} VERBOSE=1",
 
 		"mkdir -p {buildDir}/cycles/include",
 		"cd src && find . -name '*.h' | cpio -pdm {buildDir}/cycles/include",
 		"cp -r third_party/atomic/* {buildDir}/cycles/include",
-		"cp -r build/bin {buildDir}/cycles ",
+		"mkdir -p {buildDir}/cycles/bin",
+		"mv {buildDir}/cycles/cycles {buildDir}/cycles/bin/cycles",
 		"cp -r build/lib {buildDir}/cycles",
 
 	],
