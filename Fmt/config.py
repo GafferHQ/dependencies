b72117be4a090627b4b7493380067deb1d4e3042
Eric Mehl <eric@thinkhypothetical.com>
2023-02-08 17:40:24 -0500

    Fmt : add Windows configuration

--- Fmt/config.py
+++ Fmt/config.py
@@ -27,8 +27,27 @@
 	"manifest" : [
 
 		"include/fmt",
-		"lib/libfmt*{sharedLibraryExtension}*",
+		"{sharedLibraryDir}/{libraryPrefix}fmt*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}fmt*.lib"
 
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+			"mkdir build",
+			"cd build && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_INSTALL_LIBDIR={buildDir}/lib"
+				" -D BUILD_SHARED_LIBS=ON"
+				" -D FMT_TEST=OFF "
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+		],
+
+	}
 
 }
