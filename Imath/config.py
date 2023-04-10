2b150bd3722f4724c3e945052ae58588997744b7
Eric Mehl <eric@thinkhypothetical.com>
2023-04-10 17:37:45 -0400

    Imath : Add Windows configuration

--- Imath/config.py
+++ Imath/config.py
@@ -46,10 +46,45 @@
 	"manifest" : [
 
 		"include/Imath",
-		"lib/libImath*{sharedLibraryExtension}*",
-		"lib/libPyImath*{sharedLibraryExtension}*",
-		"python/imath.so",
+		"{sharedLibraryDir}/{libraryPrefix}Imath*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}Imath*.lib",
+		"{sharedLibraryDir}/{libraryPrefix}PyImath*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}PyImath*.lib",
+		"python/imath{pythonModuleExtension}",
 
 	],
+
+	"platform:windows" : {
+
+		"variables" : {
+
+			"cmakeGenerator" : "\"Visual Studio 17 2022\"",
+
+		},
+
+		"dependencies" : [ "Python", "Boost", ],
+
+		"commands" : [
+			"mkdir gafferBuild",
+			"cd gafferBuild && cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D IMATH_LIB_SUFFIX="
+				" -D PYTHON=ON"
+				" -D Boost_NO_SYSTEM_PATHS=TRUE"
+				" -D Boost_NO_BOOST_CMAKE=TRUE"
+				" -D BOOST_ROOT={buildDir}"
+				" -D Python3_ROOT_DIR={buildDir}"
+				" -D Python_NO_SYSTEM_PATHS=TRUE"
+				" -D Python3_FIND_STRATEGY=LOCATION"
+				" -D CMAKE_CXX_FLAGS=\"-DBOOST_ALL_NO_LIB -DHAVE_SNPRINTF\""
+				" -D PYIMATH_OVERRIDE_PYTHON_INSTALL_DIR={buildDir}/python"
+				" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install",
+		],
+
+},
 
 }
