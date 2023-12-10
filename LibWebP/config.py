0ec655a2b3ce1a4b22eb4556affe5e1e4ab8c983
Alex Fuller <boberfly@gmail.com>
2023-12-11 00:40:10 +1100

    LibWebP - Add version 1.3.2

---
+++ LibWebP/config.py
@@ -0,0 +1,28 @@
+{
+
+	"downloads" : [
+
+		"https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.3.2.tar.gz"
+
+	],
+
+	"url" : "https://chromium.googlesource.com/webm/libwebp",
+	"license" : "COPYING",
+
+	"commands" : [
+
+		"./configure --prefix={buildDir}",
+		"make -j {jobs}",
+		"make install",
+
+	],
+
+	"manifest" : [
+
+		"include/webp",
+		"lib/libwebp{sharedLibraryExtension}*",
+		"lib/libwebpdemux{sharedLibraryExtension}*",
+
+	],
+
+}
