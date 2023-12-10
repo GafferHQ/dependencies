9b9bddcb018f22a290979ec9f2044ec3c34259f3
Alex Fuller <boberfly@gmail.com>
2023-12-10 13:41:38 +1100

    OpenImageIO - Disable ptex, it comes with ci-vfxall in aswf-docker and gets detected from /usr/local

--- OpenImageIO/config.py
+++ OpenImageIO/config.py
@@ -30,6 +30,7 @@
 			" -D CMAKE_PREFIX_PATH={buildDir}"
 			" -D USE_FFMPEG=NO"
 			" -D USE_GIF=0"
+			" -D USE_PTEX=0"
 			" -D USE_WEBP=0"
 			" -D USE_PYTHON=YES"
 			" -D USE_EXTERNAL_PUGIXML=YES"
