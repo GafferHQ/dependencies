bcbb5d0a2ef8e3105369329266320f0e6594fe6e
Eric Mehl <eric@thinkhypothetical.com>
2024-02-09 09:10:01 -0500

    WebP : Add Windows configuration

--- LibWebP/config.py
+++ LibWebP/config.py
@@ -26,4 +26,32 @@
 
 	],
 
+	"platform:windows" : {
+
+		"manifest" : [
+
+			"include/webp",
+			"bin/libwebp{sharedLibraryExtension}*",
+			"bin/libwebpdemux{sharedLibraryExtension}*",
+			"bin/libsharpyuv{sharedLibraryExtension}*",
+			"lib/libwebp.lib",
+			"lib/libwebpdemux.lib",
+			"lib/libsharpyuv.lib",
+
+		],
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild && cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D BUILD_SHARED_LIBS=ON"
+				" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install",
+		]
+
+	}
+
 }
