ericmehl/gafferDependencies

Fmt : add Windows configuration

Fmt/config.py
@@ -27,8 +27,28 @@
 	"manifest" : [
 
 		"include/fmt",
-		"lib/libfmt*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}fmt*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}fmt*.lib"
 
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+			"mkdir build",
+			"cd build && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_INSTALL_LIBDIR={buildDir}/lib"
+				" -D BUILD_SHARED_LIBS=ON"
+				" -D FMT_TEST=OFF "
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+			"move {buildDir}\\bin\\fmt.dll {buildDir}\\lib"
+		],
+
+	}
 
 }

GafferHQ/dependencies

Fmt/config.py
@@ -27,8 +27,27 @@
 	"manifest" : [
 
 		"include/fmt",
-		"lib/libfmt*{sharedLibraryExtension}*",
+		"bin/{libraryPrefix}fmt*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}fmt*.lib"
 
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+			"mkdir build",
+			"cd build && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_INSTALL_LIBDIR={buildDir}/lib"
+				" -D BUILD_SHARED_LIBS=ON"
+				" -D FMT_TEST=OFF "
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+		],
+
+	}
 
 }