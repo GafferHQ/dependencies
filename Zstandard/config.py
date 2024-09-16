65b866d5e162458fbfefa696b0f4ca11444733e0
Eric Mehl <eric@thinkhypothetical.com>
2024-09-16 13:16:07 -0400

    Zstandard : Add Windows configuration

--- Zstandard/config.py
+++ Zstandard/config.py
@@ -30,8 +30,30 @@
 	"manifest" : [
 
 		"include/zstd.h",
-		"lib/libzstd*{sharedLibraryExtension}*"
+		"{sharedLibraryDir}/{libraryPrefix}zstd*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}zstd*.lib",
 
 	],
+	
+	"platform:windows" : {
+		
+		"commands" : [
+			
+			"mkdir gafferBuild",
+			"cd gafferBuild && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_CXX_STANDARD={c++Standard}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_INSTALL_LIBDIR={buildDir}/lib"
+				" -D CMAKE_BUILD_TYPE=Release"
+				" -D ZSTD_BUILD_PROGRAMS=OFF"
+				" -D ZSTD_BUILD_TESTS=OFF"
+				" ../build/cmake",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+		]
+	}
 
 }
