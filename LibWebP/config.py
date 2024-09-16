5d191a2ba40780d61c7506567986348c01438156
Eric Mehl <eric@thinkhypothetical.com>
2024-09-16 11:51:44 -0400

    LibWebP : Add Windows configuration

--- LibWebP/config.py
+++ LibWebP/config.py
@@ -20,10 +20,31 @@
 	"manifest" : [
 
 		"include/webp",
-		"lib/libwebp*{sharedLibraryExtension}*",
-		"lib/libwebpdemux*{sharedLibraryExtension}*",
-		"lib/libsharpyuv*{sharedLibraryExtension}*",
+		"{sharedLibraryDir}/libwebp*{sharedLibraryExtension}*",
+		"{sharedLibraryDir}/libwebpdemux*{sharedLibraryExtension}*",
+		"{sharedLibraryDir}/libsharpyuv*{sharedLibraryExtension}*",
+		"lib/libwebp*.lib",
+		"lib/libsharpyuv*.lib",
 
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+
+			"nmake /f Makefile.vc CFG=release-dynamic RTLIBCFG=dynamic objdir=output"
+
+		],
+
+		"postMovePaths" :  {
+			"output/release-dynamic/x64/bin/libwebp*.dll" : "{buildDir}/bin",
+            "output/release-dynamic/x64/lib/libwebp*.lib" : "{buildDir}/lib",
+            "output/release-dynamic/x64/bin/libsharpyuv*.dll" : "{buildDir}/bin",
+            "output/release-dynamic/x64/lib/libsharpyuv*.lib" : "{buildDir}/lib",
+			"sharpyuv/*.h" : "{buildDir}/include/webp/sharpyuv",
+			"src/webp/*.h" : "{buildDir}/include/webp",
+		},
+
+	}
 
 }
