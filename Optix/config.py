98d02d3a93218dcc137537845a948490e770feac
Eric Mehl <eric@thinkhypothetical.com>
2025-11-14 17:19:38 -0500

    Optix : Add project configuration

---
+++ Optix/config.py
@@ -0,0 +1,35 @@
+{
+
+	"downloads" : [
+
+		"https://github.com/NVIDIA/optix-dev/archive/refs/tags/v7.3.0.zip",
+
+	],
+
+	"url" : "https://developer.nvidia.com/rtx/ray-tracing/optix",
+
+	"license" : "LICENSE.txt",
+
+	"platform:macos" : {
+
+		"eanbled" : False
+
+	},
+
+	"platform:linux" : {
+
+		"eanbled" : False
+
+	},
+
+	"commands" : [],
+
+	"postMovePaths" : {
+
+		"include/**" : "{buildDir}/include",
+
+	},
+
+	"manifest" : [],
+
+}
